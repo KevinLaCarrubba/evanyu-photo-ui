41ff5c31f27472d41a00d968cdbb6f4d73835ada
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2022-10-18 12:50:42 -0400

    Nav Comlete

---
+++ components/nav/Desktop/DesktopNav.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import { NavItems } from '../routes'
+
+import styles from './DesktopNav.module.scss'
+
+import { BsInstagram, BsLinkedin } from 'react-icons/bs'
+import Logo from '../../../assets/Evan+Yu+Photography+-+Black+(Free+Crop).png'
+
+const DesktopNav = () => {
+    const router = useRouter()
+    const { pathname: currentPage } = router
+
+    return (
+        <nav className={styles.navContents}>
+            <Image src={Logo} alt="Evan Yu Photography" width={175} />
+            <ul className={styles.linkGroups}>
+                {NavItems.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <Link href={item.url}>
+                                <a
+                                    className={
+                                        currentPage === item.url
+                                            ? styles.activeLink
+                                            : ''
+                                    }>
+                                    {item.label}
+                                </a>
+                            </Link>
+                        </li>
+                    )
+                })}
+                <li>
+                    <Link href="https://www.instagram.com">
+                        <a>
+                            <BsInstagram className={styles.socialIcons} />
+                        </a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="https://www.linkedin.com">
+                        <a>
+                            <BsLinkedin className={styles.socialIcons} />
+                        </a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+export default DesktopNav
---
+++ components/nav/Mobile/mobileNav.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import styles from './MobileNav.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
+import Logo from '../../../assets/Evan+Yu+Photography+-+Black+(Free+Crop).png'
+import { MobileNavItems } from '../routes'
+const MobileNav = () => {
+    const [show, setShow] = useState(false)
+    const menuToggle = () => {
+        setShow(!show)
+        return console.log('yerrrr')
+    }
+
+    return (
+        <nav>
+            <div
+                className={
+                    show === true
+                        ? `${styles.header} ${styles.menuOpened}`
+                        : `${styles.header}`
+                }>
+                <div className={styles.burgerContainer} onClick={menuToggle}>
+                    <div className={styles.burger}>
+                        <div className={`${styles.bar} ${styles.topBar}`}></div>
+                        <div className={`${styles.bar} ${styles.btmBar}`}></div>
+                    </div>
+                </div>
+                <div className={`${styles.icon} ${styles.iconApple}`}>
+                    <Image
+                        src={Logo}
+                        alt="Evan Yu Photography"
+                        width={110}
+                        height={35}
+                    />
+                </div>
+                <ul
+                    className={styles.menu}
+                    style={{ display: `${show === true ? 'block' : 'none'}` }}>
+                    {MobileNavItems.map((item, index) => {
+                        return (
+                            <li key={index} className={styles.menuItem}>
+                                <Link href={item.url}>
+                                    <a>{item.label}</a>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <div
+                    className={`${styles.shop} ${styles.icon} ${styles.iconBag}`}></div>
+            </div>
+        </nav>
+    )
+}
+export default MobileNav
