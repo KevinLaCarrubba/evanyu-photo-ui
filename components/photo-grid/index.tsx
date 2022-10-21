cf0ae53b3c34dd052c2903496657a08babb0670e
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2022-10-21 16:56:32 -0400

    trial

--- components/photo-grid/index.tsx
+++ components/photo-grid/index.tsx
@@ -6,40 +6,23 @@ import styles from './PhotoGrid.module.scss'
 import Image from 'next/image'
 
 const PhotoGrid = (urlArray: any) => {
-    const cardVariants: Variants = {
-        offscreen: {
-            filter: 'blur(2px)',
-            scale: 0.9
-        },
-        onscreen: {
-            filter: 'blur(0)',
-            scale: 1,
-            transition: {
-                duration: 0.3
-            }
-        }
-    }
-
     return (
         <>
             <div className={styles.pageContent}>
-                <ImageList variant="masonry" cols={3} gap={10}>
-                    {urlArray?.urlArray.map((item: string, index: number) => (
-                        <ImageListItem key={index}>
-                            <Image
-                                src={`${item}?w=161&fit=crop&auto=format`}
-                                blurDataURL={`${item}?w=161&fit=crop&auto=format`}
-                                // srcSet={`${item}?w=161&fit=crop&auto=format&dpr=2 2x`}
-                                width="100%"
-                                height="100%"
-                                layout="responsive"
-                                loading="lazy"
-                                placeholder="blur"
-                                objectFit="cover"
-                            />
-                        </ImageListItem>
-                    ))}
-                </ImageList>
+                {urlArray?.urlArray.map((item: string, index: number) => (
+                    <Image
+                        key={index}
+                        src={`${item}?w=161&fit=crop&auto=format`}
+                        blurDataURL={`${item}?w=161&fit=crop&auto=format`}
+                        // srcSet={`${item}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                        width={850}
+                        height={1200}
+                        layout="responsive"
+                        loading="lazy"
+                        placeholder="blur"
+                        objectFit="cover"
+                    />
+                ))}
             </div>
         </>
     )
