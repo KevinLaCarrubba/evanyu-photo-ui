85fcf26221abd6e5a8a437885c4897636baf54a2
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2022-10-21 17:30:06 -0400

    mobile photo grid

--- components/photo-grid/index.tsx
+++ components/photo-grid/index.tsx
@@ -1,7 +1,3 @@
-import ImageList from '@mui/material/ImageList'
-import ImageListItem from '@mui/material/ImageListItem'
-import { motion, Variants } from 'framer-motion'
-
 import styles from './PhotoGrid.module.scss'
 import Image from 'next/image'
 
@@ -14,7 +10,6 @@ const PhotoGrid = (urlArray: any) => {
                         key={index}
                         src={`${item}?w=161&fit=crop&auto=format`}
                         blurDataURL={`${item}?w=161&fit=crop&auto=format`}
-                        // srcSet={`${item}?w=161&fit=crop&auto=format&dpr=2 2x`}
                         width={850}
                         height={1200}
                         layout="responsive"
